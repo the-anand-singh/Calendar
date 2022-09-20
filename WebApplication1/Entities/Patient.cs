1b66309782d8207916ba5384607d9b0aabcf5d0d
Komalshekhawat <75672726+Komalshekhawat@users.noreply.github.com>
2022-09-20 09:32:46 +0530

    entities

---
+++ WebApplication1/Entities/Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Entities
+{
+    [Table("Patient")]
+    public class Patient
+    {
+        [Key]
+        public int PatientID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public string MobileNo { get; set; }
+        public string BloodGroup { get; set; }
+
+       
+    }
+}
